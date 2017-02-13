ad4eaa4a87fda9a1b86ea646f5dc0d80347dcb9c
Scott Fox <scottfox5@gmail.com>
2017-02-13 11:59:32 -0600

    Initial Commit

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ app.use('/auth', auth);
 app.use('/private', isLoggedIn, private);
 app.use('/', index);
 /** ---------- SERVER START ---------- **/
-app.listen(3000, function () {
-  console.log('Now running on port ', 3000);
+app.listen(5555, function () {
+  console.log('Now running on port ', 5555);
 });
