230dc43090642aa4673937f4b31237b59ceb143e
Scott Fox <scottfox5@gmail.com>
2017-02-19 17:11:08 -0600

    turned of console logs

--- public/scripts/services/journalService.js
+++ public/scripts/services/journalService.js
@@ -2,7 +2,7 @@ googleAuthApp.service('JournalService', function ($http) {
 
   this.getUserJournal = function() {
     return $http.get("/private/journal").then(function(response) {
-      console.log('getting user journal:', response, response.data)
+      // console.log('get user journal:', response.data)
       return response.data;
     }).catch(function(err) {
       console.log("Error getting user journal", err);
@@ -10,26 +10,29 @@ googleAuthApp.service('JournalService', function ($http) {
   };
 
   this.addJournalEntry = function(journalEntry) {
-    console.log('add journal entry:', journalEntry)
+    // console.log('add journal entry:', journalEntry)
     return $http.post("/private/journal", journalEntry)
     .catch(function(err) {
       console.log("Error saving journal entry", err);
     });
   };
 
+  this.updateJournalEntry = function(id, data) {
+    // console.log('update data:', data);
+    return $http.put("/private/journal/" + id, data)
+    .catch(function(err) {
+      console.log("Error updating journal entry", err);
+    });
+  };
+
   this.deleteJournalEntry = function(id) {
-    console.log ('delete id:', id)
+    // console.log ('delete id:', id)
     return $http.delete("/private/journal/" + id)
     .catch(function(err) {
       console.log("Error deleting journal entry", err);
     });
   };
 
-  this.updateJournalEntry = function(id, data) {
-    console.log('update data:', data);
-    return $http.put("/private/journal/" + id, data)
-    .catch(function(err) {
-      console.log("Error updating journal entry", err);
-    });
-  };
-});
+
+
+}); // end of journal service
