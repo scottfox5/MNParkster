f44c407464b6514374c00bdb5cce875ff62d1847
Scott Fox <scottfox5@gmail.com>
2017-02-27 00:50:40 -0600

    added initial code for google maps api

--- public/scripts/controllers/weatherController.js
+++ public/scripts/controllers/weatherController.js
@@ -48,4 +48,86 @@ parkApp.controller('WeatherController', function($http){
     });
   };
 
+
+// //  google maps distance matrix api code
+//
+// function initMap() {
+//   var bounds = new google.maps.LatLngBounds;
+//   var markersArray = [];
+//
+//   var origin = 'Minneapolis, MN';
+//   // var destination = {lat: 47.1975, lng: -95.20167};
+//   var destination = 'Hill-Annex Mine State Park, MN';
+//
+//
+//
+//   var destinationIcon = 'https://chart.googleapis.com/chart?' +
+//       'chst=d_map_pin_letter&chld=D|FF0000|000000';
+//   var originIcon = 'https://chart.googleapis.com/chart?' +
+//       'chst=d_map_pin_letter&chld=O|FFFF00|000000';
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 10
+//   });
+//   var geocoder = new google.maps.Geocoder;
+//
+//   var service = new google.maps.DistanceMatrixService;
+//   service.getDistanceMatrix({
+//     origins: [origin],
+//     destinations: [destination],
+//     travelMode: 'DRIVING',
+//     unitSystem: google.maps.UnitSystem.IMPERIAL,
+//     avoidHighways: false,
+//     avoidTolls: false
+//   }, function(response, status) {
+//     if (status !== 'OK') {
+//       alert('Error was: ' + status);
+//     } else {
+//       var originList = response.originAddresses;
+//       var destinationList = response.destinationAddresses;
+//       var outputDiv = document.getElementById('output');
+//       outputDiv.innerHTML = '';
+//       deleteMarkers(markersArray);
+//
+//       var showGeocodedAddressOnMap = function(asDestination) {
+//         var icon = asDestination ? destinationIcon : originIcon;
+//         return function(results, status) {
+//           if (status === 'OK') {
+//             map.fitBounds(bounds.extend(results[0].geometry.location));
+//             markersArray.push(new google.maps.Marker({
+//               map: map,
+//               position: results[0].geometry.location,
+//               icon: icon
+//             }));
+//           } else {
+//             alert('Geocode was not successful due to: ' + status);
+//           }
+//         };
+//       };
+//
+//       for (var i = 0; i < originList.length; i++) {
+//         var results = response.rows[i].elements;
+//         geocoder.geocode({'address': originList[i]},
+//             showGeocodedAddressOnMap(false));
+//         for (var j = 0; j < results.length; j++) {
+//           geocoder.geocode({'address': destinationList[j]},
+//               showGeocodedAddressOnMap(true));
+//           outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
+//               ': ' + results[j].distance.text + ' in ' +
+//               results[j].duration.text + '<br>';
+//         }
+//       }
+//     }
+//   });
+// }
+//
+// function deleteMarkers(markersArray) {
+//   for (var i = 0; i < markersArray.length; i++) {
+//     markersArray[i].setMap(null);
+//   }
+//   markersArray = [];
+// }
+
+
+
+
 }); // end of weather controller
