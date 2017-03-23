0393375aa14e31146b8202c2001667cc03282606
Scott Fox <scottfox5@gmail.com>
2017-03-23 14:54:12 -0500

    heroku deploy

--- routes/private/quote.js
+++ routes/private/quote.js
@@ -4,15 +4,22 @@ var QuoteBook = require('../../models/quotes');
 
   router.get('/', function (req, res) {
     QuoteBook.find({}, function (err, data) {
-      var quoteArray = data[0].quotes;
-      // selecting random item from array
-      var quote = quoteArray[Math.floor(Math.random()*quoteArray.length)];
-      // console.log('Quote:', quote)
+      if (data.length == 0){
+        res.sendStatus(500)
+        return
+      } else {
+        var quoteArray = data[0].quotes;
+        // selecting random item from array
+        var quote = quoteArray[Math.floor(Math.random()*quoteArray.length)];
+        // console.log('Quote:', quote)
+        res.send(quote);
+        return
+      }
       if (err) {
         res.sendStatus(500);
         return;
       }
-      res.send(quote);
+
     });
   }); // end of get
 
