8ea8002bb2e343ad1750a66130fe812f0a44a882
Scott Fox <scottfox5@gmail.com>
2017-03-23 12:00:19 -0500

    corrected callback url in config/auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,8 @@ var authConfigs = {
   googleAuth: {
     clientId: '853875757633-23t5cdbrckt3c444atnntnjhg4akot9o.apps.googleusercontent.com',
     clientSecret: '5_q40m2PIgyZk3Ym21Nf8mzT',
-    callbackUrl: 'http://localhost:5555/auth/google/callback',
+    // callbackUrl: 'http://localhost:5555/auth/google/callback',
+    callbackUrl: 'https://gentle-retreat-78418.herokuapp.com/#/login/auth/google/callback',
   },
 
   sessionVars: {
