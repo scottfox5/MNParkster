3cc911da55d9d26ccd78b2246038c735705e0708
Scott Fox <scottfox5@gmail.com>
2017-03-23 11:51:50 -0500

    allowed config/auth.js for heroku deployment

---
+++ config/_auth.js
@@ -0,0 +1,13 @@
+var authConfigs = {
+  googleAuth: {
+    clientId: '',
+    clientSecret: '',
+    callbackUrl: 'http://localhost:5555/auth/google/callback',
+  },
+
+  sessionVars: {
+    secret: '',
+  },
+};
+
+module.exports = authConfigs;
