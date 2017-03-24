6c735ef0a85d04611372e14825e88c4bd24330ee
Scott Fox <scottfox5@gmail.com>
2017-03-24 09:45:30 -0500

    testing checklilst

--- routes/private/checklist.js
+++ routes/private/checklist.js
@@ -21,7 +21,7 @@ router.get('/', function (req, res) {
       parksArray.forEach(function(i) { count[i] = (count[i]||0)+1;  }); // converts array of items to object with distinct item and number of occurences in array
       console.log('List of unique parks and total visits:', count)
       var parksVisited = Object.keys(count); // puts keys into array
-      var parkVisits = Object.values(count); // puts values into array
+      // var parkVisits = Object.values(count); // puts values into array
 
       // converting two arrays into array of objects with properties of parks and visits
       var checklist = parksVisited.map(function(e,i){return{park:e,visits:parkVisits[i]}});
