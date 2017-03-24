e726c770fb1c9ab7b174e53088d5eb525b6b8399
Scott Fox <scottfox5@gmail.com>
2017-03-24 09:56:40 -0500

    testing checklist

--- routes/private/checklist.js
+++ routes/private/checklist.js
@@ -1,3 +1,48 @@
+// var express = require('express');
+// var router = express.Router();
+// var UserJournal = require('../../models/user');
+//
+// router.get('/', function (req, res) {
+//
+//   UserJournal.find({"_id" : req.user._id}, function (err, data) {
+//     if (data.length == 0){
+//       res.sendStatus(500)
+//       return
+//     } else {
+//       console.log('User Data:', data)
+//       var journalData = data[0].journal;
+//
+//       // converting park propery of array of journal entry objects into array of parks
+//       var parksArray = journalData.map(function(item) { return item["park"]; });
+//       parksArray.sort();
+//       console.log('Parks Array:', parksArray)
+//       // counting number of each item in parks array and converting to two arrays: one of distinct parks, one of number of visits to each park
+//       var  count = {};
+//       parksArray.forEach(function(i) { count[i] = (count[i]||0)+1;  }); // converts array of items to object with distinct item and number of occurences in array
+//       console.log('List of unique parks and total visits:', count)
+//       var parksVisited = Object.keys(count); // puts keys into array
+//       var parkVisits = Object.values(count); // puts values into array
+//
+//       // converting two arrays into array of objects with properties of parks and visits
+//       var checklist = parksVisited.map(function(e,i){return{park:e,visits:parkVisits[i]}});
+//       console.log('Checklist Array of Objects w/ park/visits:', checklist)
+//       res.send(checklist);
+//       return
+//     }
+//     if (err) {
+//       res.sendStatus(500);
+//       return;
+//     }
+//
+//   }); // end of UserJournal.find
+//
+// }); // end of get
+//
+//
+// module.exports = router;
+
+
+
 var express = require('express');
 var router = express.Router();
 var UserJournal = require('../../models/user');
@@ -5,36 +50,33 @@ var UserJournal = require('../../models/user');
 router.get('/', function (req, res) {
 
   UserJournal.find({"_id" : req.user._id}, function (err, data) {
-    if (data.length == 0){
-      res.sendStatus(500)
-      return
-    } else {
-      console.log('User Data:', data)
-      var journalData = data[0].journal;
+    // console.log('User Data:' data)
+    var journalData = data[0].journal;
 
-      // converting park propery of array of journal entry objects into array of parks
-      var parksArray = journalData.map(function(item) { return item["park"]; });
-      parksArray.sort();
-      console.log('Parks Array:', parksArray)
-      // counting number of each item in parks array and converting to two arrays: one of distinct parks, one of number of visits to each park
-      var  count = {};
-      parksArray.forEach(function(i) { count[i] = (count[i]||0)+1;  }); // converts array of items to object with distinct item and number of occurences in array
-      console.log('List of unique parks and total visits:', count)
-      var parksVisited = Object.keys(count); // puts keys into array
-      // var parkVisits = Object.values(count); // puts values into array
+    // converting park propery of array of journal entry objects into array of parks
+    var parksArray = journalData.map(function(item) { return item["park"]; });
+    parksArray.sort();
 
-      // converting two arrays into array of objects with properties of parks and visits
-      var checklist = parksVisited.map(function(e,i){return{park:e,visits:parkVisits[i]}});
-      console.log('Checklist Array of Objects w/ park/visits:', checklist)
-      res.send(checklist);
-      return
-    }
+    // counting number of each item in parks array and converting to two arrays: one of distinct parks, one of number of visits to each park
+    var  count = {};
+    parksArray.forEach(function(i) { count[i] = (count[i]||0)+1;  }); // converts array of items to object with distinct item and number of occurences in array
+    //console.log('List of unique parks and total visits:', count)
+    var parksVisited = Object.keys(count); // puts keys into array
+    var parkVisits = Object.values(count); // puts values into array
+
+    // converting two arrays into array of objects with properties of parks and visits
+    var checklist = parksVisited.map(function(e,i){return{park:e,visits:parkVisits[i]}});
+
     if (err) {
+
       res.sendStatus(500);
       return;
     }
+    // console.log('Checklist Array of Objects w/ park/visits:', checklist)
+    res.send(checklist);
 
   }); // end of UserJournal.find
+
 
 }); // end of get
 
