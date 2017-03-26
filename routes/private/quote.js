d4b318950bbf2986f07ff0f30ddfaff858cc4b51
Scott Fox <scottfox5@gmail.com>
2017-03-26 14:04:41 -0500

    quote testign

--- routes/private/quote.js
+++ routes/private/quote.js
@@ -4,17 +4,17 @@ var QuoteBook = require('../../models/quotes');
 
   router.get('/', function (req, res) {
     QuoteBook.find({}, function (err, data) {
-      console.log("Quote Data:", data)
+      // console.log("Quote Data:", data)
       if (data.length == 0){
         res.sendStatus(500)
         return
       } else {
         var quoteArray = data[0].quotes;
-        console.log("Quote Array:", quoteArray);
+        // console.log("Quote Array:", quoteArray);
 
         // selecting random item from array
         var quote = quoteArray[Math.floor(Math.random()*quoteArray.length)];
-        console.log('Quote:', quote)
+        // console.log('Quote:', quote)
         res.send(quote);
         return
       }
