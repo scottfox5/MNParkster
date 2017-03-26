894e4026dbdb346f0ac8098a6493837c5312f74d
Scott Fox <scottfox5@gmail.com>
2017-03-26 13:49:10 -0500

    fixed call for quote array

--- routes/private/quote.js
+++ routes/private/quote.js
@@ -9,9 +9,9 @@ var QuoteBook = require('../../models/quotes');
         res.sendStatus(500)
         return
       } else {
-        var quoteArray = data[0].quotes;
+        var quoteArray = data[1].quotes;
         console.log("Quote Array:", quoteArray);
-        console.log("data[1].quotes:", data[1].quotes);
+        
         // selecting random item from array
         var quote = quoteArray[Math.floor(Math.random()*quoteArray.length)];
         console.log('Quote:', quote)
