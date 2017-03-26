92438a28ae285b44cbe97e37f0c1c3e684625d07
Scott Fox <scottfox5@gmail.com>
2017-03-26 13:52:28 -0500

    testing quote

--- routes/private/quote.js
+++ routes/private/quote.js
@@ -9,9 +9,9 @@ var QuoteBook = require('../../models/quotes');
         res.sendStatus(500)
         return
       } else {
-        var quoteArray = data[1].quotes;
+        var quoteArray = data[0].quotes;
         console.log("Quote Array:", quoteArray);
-        
+
         // selecting random item from array
         var quote = quoteArray[Math.floor(Math.random()*quoteArray.length)];
         console.log('Quote:', quote)
