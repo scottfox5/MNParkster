ed1d2a291bfa8f19afed4c5a9ad967dc80977ca3
Scott Fox <scottfox5@gmail.com>
2017-03-26 13:06:31 -0500

    removed Object.values: testing other JS

--- routes/private/checklist.js
+++ routes/private/checklist.js
@@ -65,14 +65,16 @@ router.get('/', function (req, res) {
     parksArray.forEach(function(i) { count[i] = (count[i]||0)+1;  }); // converts array of items to object with distinct item and number of occurences in array
     console.log('List of unique parks and total visits:', count);
 
-    var countDuplicate = count;
-    console.log("Count Duplicate:", countDuplicate)
-
     var parksVisited = Object.keys(count); // puts keys into array
     console.log("Parks Visited:", parksVisited);
 
-    var parkVisits = Object.values(countDuplicate); // puts values into array
+    // var parkVisits = Object.values(count); // puts values into array
+    // console.log("Park Visits:", parkVisits);
+    var parkVisits = Object.keys(count).map(function(key) {
+    return count[key];
+    });
     console.log("Park Visits:", parkVisits);
+
 
     // converting two arrays into array of objects with properties of parks and visits
     var checklist = parksVisited.map(function(e,i){return{park:e,visits:parkVisits[i]}});
