7f881b4572e55b37e0282fd1c7261d97bc73e9b7
Scott Fox <scottfox5@gmail.com>
2017-03-26 14:44:10 -0500

    updated callback

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ var authConfigs = {
     clientId: '853875757633-23t5cdbrckt3c444atnntnjhg4akot9o.apps.googleusercontent.com',
     clientSecret: '5_q40m2PIgyZk3Ym21Nf8mzT',
     // callbackUrl: 'http://localhost:5555/auth/google/callback',
-    callbackUrl: 'https://gentle-retreat-78418.herokuapp.com/auth/google/callback',
+    callbackUrl: 'https://mn-parkster.herokuapp.com/auth/google/callback',
   },
 
   sessionVars: {
