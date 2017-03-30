1a1ef364beefa00a93d0917c953f6b8ad71f95fb
Scott Fox <scottfox5@gmail.com>
2017-03-30 13:57:32 -0500

    Delete database.js

--- config/database.js
+++
@@ -1,4 +0,0 @@
-// NOTE Location of mongo database : Use location below when serving on localhost, mn-parks refers to mongo database name
-// module.exports = 'mongodb://localhost:27017/mn-parks';
-
-module.exports = 'mongodb://heroku_3t22m0km:l4981o23jcadpvp9cum0m7m9rq@ds139360.mlab.com:39360/heroku_3t22m0km';
