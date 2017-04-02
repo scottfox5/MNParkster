71b358b60ec645616e6c1a284723cf200fc388ce
Scott Fox <scottfox5@gmail.com>
2017-04-02 17:12:45 -0500

    heroku update

--- config/auth.js
+++ config/auth.js
@@ -2,8 +2,8 @@ var authConfigs = {
   googleAuth: {
     clientId: '853875757633-23t5cdbrckt3c444atnntnjhg4akot9o.apps.googleusercontent.com',
     clientSecret: '5_q40m2PIgyZk3Ym21Nf8mzT',
-    // callbackUrl: 'http://localhost:5555/auth/google/callback', // to run locally
-    callbackUrl: 'https://mn-parkster.herokuapp.com/auth/google/callback', // to run on heroku
+    callbackUrl: 'http://localhost:5555/auth/google/callback', // to run locally
+    // callbackUrl: 'https://mn-parkster.herokuapp.com/auth/google/callback', // to run on heroku
   },
 
   sessionVars: {
